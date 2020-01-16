929d41f43d02daec9512e85a3f57515607a63659
Eivy <modern.times.rock.and.roll+git@gmail.com>
2020-01-16 14:37:40 +0900

    Update for example

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/ns217476/Code/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/ns217476/Code/src/github.com/Eivy/get_proxy/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/ns217476/Code/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
